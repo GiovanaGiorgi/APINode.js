9f45824c655990e469370ac7103ef4b9a22a77ab
giovana <132526932+GiovanaGiorgi@users.noreply.github.com>
2024-08-17 20:34:38 -0300

    Configuração das rotas e alteração do server.js

---
+++ routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const enderecoController = require('./controllers/enderecoController');
+
+const router = express.Router();
+
+router.post('/enderecos', enderecoController.createEndereco);
+router.get('/enderecos', enderecoController.getAllEnderecos);
+router.get('/enderecos/:id', enderecoController.getEnderecoById);
+router.put('/enderecos/:id', enderecoController.updateEndereco);
+router.delete('/enderecos', enderecoController.deleteEndereco);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,44 @@
 const express = require('express');
-const axios = require('axios')
-
-const cepRegex = /^[0-9]{5}-?[09]{3}$/;
+const rotas = require('./routes');
 
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
+app.set(express.json());
+app.use('/api', rotas);
 
-app.get('/rota', (req, res) => {
-    res.send('Route 66');
-});
 
-app.get('/consulta-cep/:cep', async (req, res) => {
-    const cep = req.params.cep;
-    if (!cepRegex.test(cep)) {
-        res.status(400).send('CEP inválido. Formato: XXXXX-XXX');
-    }
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${port}`));
 
-    try{
-        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-        res.json(response.data);
-    } catch (error) {
-        console.error('Erro ao fazer a requisição:', error);
-        res.status(500).send('Erro ao consultar o CEP');
-    }
-});
+// const cepRegex = /^[0-9]{5}-?[09]{3}$/;
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-});
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//     res.send('Hello world');
+// });
+
+// app.get('/rota', (req, res) => {
+//     res.send('Route 66');
+// });
+
+// app.get('/consulta-cep/:cep', async (req, res) => {
+//     const cep = req.params.cep;
+//     if (!cepRegex.test(cep)) {
+//         res.status(400).send('CEP inválido. Formato: XXXXX-XXX');
+//     }
+
+//     try{
+//         const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+//         res.json(response.data);
+//     } catch (error) {
+//         console.error('Erro ao fazer a requisição:', error);
+//         res.status(500).send('Erro ao consultar o CEP');
+//     }
+// });
+
+// app.listen(port, () => {
+//     console.log(`Servidor rodando em http://localhost:${port}`);
+// });
     
