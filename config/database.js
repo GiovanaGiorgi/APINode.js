80c1aca860763b1fd698d5ea914851ea46480baf
giovana <132526932+GiovanaGiorgi@users.noreply.github.com>
2024-08-19 19:32:18 -0300

    Correção da configuração da migration

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '131989',
+    database: 'api-node',
+    define:{
+        timestamps: true,
+        underscored: true
+    }
+}
